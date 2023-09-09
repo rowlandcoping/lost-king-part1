da8b27dab3567a8c0fb943fce663ee5aaee6222d
John Hall <rowlandcoping@gmail.com>
2023-09-09 14:26:59 +0100

    ADD: restart and reset functionality, dynamic content, skills generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,131 @@
 const mainCharacter = {
     name: "",
-    score: "0",
-    strength: "15",
-    speed: "15",
-    stamina: "15",
-    luck: "15"
+    score: "",
+    strength: "",
+    speed: "",
+    stamina: "",
+    luck: "",
+    health: ""
 };
+
+
+
+const getRandomNumber= (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+
+function generateStats(character, min, max, hMin, hMax) {
+    character.strength = getRandomNumber(min, max);
+    character.speed = getRandomNumber(min, max);
+    character.stamina = getRandomNumber(min, max);
+    character.luck = getRandomNumber(min, max);
+    character.health = getRandomNumber(hMin, hMax);
+}
+
+function resetGame() {
+    mainCharacter.name = "";
+    mainCharacter.strength = "";
+    mainCharacter.speed = "";
+    mainCharacter.stamina = "";
+    mainCharacter.luck = "";
+    mainCharacter.health = "";
+    document.getElementById('landing-page').style.display = "flex";
+    document.getElementById('game-page').style.display = "none";
+    localStorage.clear();
+}
+
 
 function startGame(event) {
     if (!mainCharacter.name) {
+        localStorage.clear();
         if (document.getElementById('character-name').value) {
         mainCharacter.name = document.getElementById('character-name').value;
-        }else{mainCharacter.name = "Another Lazy Gamer"}
+        generateStats (mainCharacter, 12, 20, 50, 100);
+        }else{
+            mainCharacter.name = "Another Lazy Gamer";
+            generateStats (mainCharacter, 12, 20, 50, 100);
+           }
     }
+    mainCharacter.score = "0";
+    writeInitialToDom();
+}
+
+function writeInitialToDom() {
     document.getElementById('landing-page').style.display = "none";
     document.getElementById('game-page').style.display = "flex";
-    document.getElementById('initial-name').innerHTML = mainCharacter.name;
     document.getElementById('main-strength').innerHTML = mainCharacter.strength;
     document.getElementById('main-speed').innerHTML = mainCharacter.speed;
     document.getElementById('main-stamina').innerHTML = mainCharacter.stamina;
     document.getElementById('main-luck').innerHTML = mainCharacter.luck;
+    document.getElementById('main-health').innerHTML = mainCharacter.health;
+    document.getElementById('game-text').innerHTML = pageOne;
+    document.getElementById('choices-section').innerHTML = optionsOne;    
 }
 
+function openEyes(){    
+    document.getElementById('game-text').innerHTML = pageTwo;
+    document.getElementById('choices-section').innerHTML = optionsTwoFirst + mainCharacter.name + optionsTwoSecond;   
+}
+
+/*start game button event handler*/
 document.addEventListener('DOMContentLoaded', function () {  
     document.getElementById('start-game-button').addEventListener('click', startGame);
 });
+/*restart character button event handler*/
+document.addEventListener('DOMContentLoaded', function () {  
+    document.getElementById('restart-game-button').addEventListener('click', startGame);
+});
+/*reset character button event handler*/
+document.addEventListener('DOMContentLoaded', function () {  
+    document.getElementById('reset-game-button').addEventListener('click', resetGame);
+});
 
+/*GAME EVENT HANDLERS*/
+
+/*page one event handlers*/
+
+    document.addEventListener("click", function(e){
+        const target = e.target.closest("#choice-one"); // Or any other selector.
+        if(target){
+            openEyes();
+        }
+ });
+
+
+/* GAME TEXT */
+
+/*page one */
+
+const pageOne = `
+    <p>You awaken.  You become aware of something.  It is pain.</p>  
+    
+    <p>You try to open your eyes.  You cannot. It feels like they are welded together.</p>
+    
+    <p>You reach for them, find a thin crust of blood. You try to prise them open.  The pain grows.</p>    
+`;
+
+const optionsOne = `
+    <li><button class="choice-button" id="choice-one">Open your eyes.</button></li>
+    <li><button class="choice-button" id="choice-two">Give up.</button></li>
+`;
+/* page two */
+const pageTwo = `
+    <p>It makes no discernable difference.  Either you are blind, or it is deep, pitch dark.</p>
+
+    <p>But who are you?</p>
+    <p>"Who am I???"</p>
+    <p>Somebody spoke the words.  You think that it was you.</p>
+`;
+
+const optionsTwoFirst = `
+    <li><button class="choice-button" id="choice-three">That's easy.  I am called... `   
+const optionsTwoSecond = `
+    </button></li>
+    <li><button class="choice-button" id="choice-four">I am The Soul Of DARKNESSSSS.</button></li>
+    <li><button class="choice-button" id="choice-five">It's...... too early to say.</button></li>
+`;
 
 
 var module = module || {};
-module.exports = { mainCharacter, startGame };
+module.exports = { mainCharacter, startGame, getRandomNumber, writeInitialToDom, generateStats, resetGame, pageOne, optionsOne };
 
 
--- assets/js/testing/script.test.js
+++ assets/js/testing/script.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { mainCharacter, startGame } = require("../script.js");
+const { mainCharacter, startGame, getRandomNumber, writeInitialToDom, generateStats, resetGame, pageOne, optionsOne } = require("../script.js");
 
 beforeAll(() => {
    let fs = require("fs");
@@ -31,25 +31,70 @@ describe("mainCharacter object contains correct properties", ()=>{
     test("luck property exists", () =>{
         expect("name" in mainCharacter).toBe(true);
     })
+    test("health property exists", () =>{
+      expect("health" in mainCharacter).toBe(true);
+  })
 });
 
-describe("start game button callback function works as intended", ()=>{
+describe("random number generator works as expected", ()=>{
+   test("should return random integer between 1 and 5", () =>{
+      expect(getRandomNumber(1,5)).toBeGreaterThanOrEqual(1);
+      expect(getRandomNumber(1,5)).toBeLessThanOrEqual(5);
+   })
+});
+
+describe("character stat creation code works as expected", ()=>{
+   beforeAll(() =>{
+      generateStats(mainCharacter, 1,5,15,25);
+   }),
+   test("mainCharacter should return strength as integer between 1 and 5", () =>{
+      expect(mainCharacter.strength).toBeGreaterThanOrEqual(1);
+      expect(mainCharacter.strength).toBeLessThanOrEqual(5);
+   }),
+   test("mainCharacter should return speed as integer between 1 and 5", () =>{
+      expect(mainCharacter.speed).toBeGreaterThanOrEqual(1);
+      expect(mainCharacter.speed).toBeLessThanOrEqual(5);
+   }),
+   test("mainCharacter should return stamina as integer between 1 and 5", () =>{
+      expect(mainCharacter.stamina).toBeGreaterThanOrEqual(1);
+      expect(mainCharacter.stamina).toBeLessThanOrEqual(5);
+   }),
+   test("mainCharacter should return luck as integer between 1 and 5", () =>{
+      expect(mainCharacter.luck).toBeGreaterThanOrEqual(1);
+      expect(mainCharacter.luck).toBeLessThanOrEqual(5);
+   }),
+   test("mainCharacter should return health as integer between 15 and 25", () =>{
+      expect(mainCharacter.health).toBeGreaterThanOrEqual(15);
+      expect(mainCharacter.health).toBeLessThanOrEqual(25);
+   })
+});
+
+
+
+describe("start game function works as intended", ()=>{
    beforeAll(() => {
       document.getElementById('character-name').value = "Randy Tangent";
+      mainCharacter.score=150;
       startGame();
    }),
    test("name field in mainCharacter correctly populated", () =>{
       expect(mainCharacter.name).toBe("Randy Tangent");
    }),
-   test("name is added to initial gameplay section", () =>{
-      expect(document.getElementById('initial-name').innerHTML).toBe("Randy Tangent");
-   }),
-   test("landing-page div is hidden", () =>{
-      expect(document.getElementById('landing-page').style.display).toBe("none");
-   }),
-   test("game-page div is set to flex", () =>{
-      expect(document.getElementById('game-page').style.display).toBe("flex");
-   }),
+   test("Score is reset to zero", () =>{
+      expect(mainCharacter.score).toEqual("0");
+   })
+
+});
+
+describe("write character info works as intended", () => {
+   beforeAll(() => {
+      mainCharacter.strength = 15;
+      mainCharacter.speed = 15;
+      mainCharacter.stamina = 15;
+      mainCharacter.luck = 15;
+      mainCharacter.health = 75;
+      writeInitialToDom();
+   })
    test("strength property written to character sheet on page", () =>{
       expect(document.getElementById('main-strength').innerHTML).toBe("15");
    }),
@@ -60,22 +105,69 @@ describe("start game button callback function works as intended", ()=>{
       expect(document.getElementById('main-stamina').innerHTML).toBe("15");
    }),
    test("luck property written to character sheet on page", () =>{
-       expect(document.getElementById('main-luck').innerHTML).toBe("15");
+      expect(document.getElementById('main-luck').innerHTML).toBe("15");
+   }),
+   test("health property written to character sheet on page", () =>{
+      expect(document.getElementById('main-health').innerHTML).toBe("75");
+   }),
+   test("landing page is hidden", () =>{
+      expect(document.getElementById('landing-page').style.display).toBe("none");
+   }),
+   test("game page is displayed", () =>{
+      expect(document.getElementById('game-page').style.display).toBe("flex");
    })
-
+   
 });
 
-/*
-describe("Intro page works as intended", ()=>{
+describe("first page text displays as intended", ()=>{
    beforeAll(() => {
-      document.getElementById('character-name').value = "Randy Tangent";
+      writeInitialToDom();
+   }),
+   test("story content appears as intended", () =>{
+      expect(document.getElementById('game-text').innerHTML).toBe(pageOne);
+   }),
+   test("story options appear as intended", () =>{
+      expect(document.getElementById('choices-section').innerHTML).toBe(optionsOne);
+   })
+});
+
+
+
+describe("restart game button works as intended", ()=>{
+   beforeAll(() => {
+      mainCharacter.name = "Randy Tangent";
       startGame();
    }),
-   test("name field in mainCharacter correctly populated", () =>{
+   test("name field in mainCharacter correctly retained", () =>{
       expect(mainCharacter.name).toBe("Randy Tangent");
-   }),
-   test("name is added to initial gameplay section", () =>{
-      expect(document.getElementById('initial-name').innerHTML).toBe("Randy Tangent");
-   }),
+   })
 });
-*/
+
+
+
+
+describe("reset character button works as intended", ()=>{
+   beforeAll(() => {
+      resetGame();
+   }),
+   test("name field in mainCharacter cleared", () =>{
+      expect(mainCharacter.name).toBe("");
+   }),
+   test("strength field in mainCharacter cleared", () =>{
+      expect(mainCharacter.name).toBe("");
+   }),
+   test("speed field in mainCharacter cleared", () =>{
+      expect(mainCharacter.name).toBe("");
+   }),
+   test("stamina field in mainCharacter cleared", () =>{
+      expect(mainCharacter.name).toBe("");
+   }),
+   test("luck field in mainCharacter cleared", () =>{
+      expect(mainCharacter.name).toBe("");
+   }),
+   test("health field in mainCharacter cleared", () =>{
+      expect(mainCharacter.name).toBe("");
+   })
+});
+
+
